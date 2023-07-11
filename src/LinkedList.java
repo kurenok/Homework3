b62f565f8400ddfb030362f3af76f66aa949c564
kurenok <limonikpuh@gmail.com>
2023-07-11 23:32:39 +0300

    Initial commit

---
+++ src/LinkedList.java
@@ -0,0 +1,39 @@
+public class LinkedList<T> {
+    private Node head; private int size;
+    private class Node {
+        T value;
+        Node next;
+        Node(T value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    public void add(T value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public int size() {
+        return size;
+    }
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.value;
+    }
+}
